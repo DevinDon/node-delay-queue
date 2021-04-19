b2c5140bf02cc55c5a96a00ad3da8634138d0972
Devin Don <i.inf@outlook.com>
2021-04-19 15:41:34 +0800

    chore(deps): update deps

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
-import { Rester } from '@rester/core';
+import { DEFAULT_HANDLERS, Rester } from '@rester/core';
 import 'reflect-metadata';
+import { AccessModule } from './access';
+import { AccessHandler } from './common/handlers';
 import { QueueModule } from './queue';
 
-// const rester = new Rester({
-//   handlers: [AccessHandler, ...DEFAULT_HANDLERS],
-//   modules: [AccessModule, QueueModule],
-// });
-
 const rester = new Rester({
-  modules: [QueueModule],
+  handlers: [AccessHandler, ...DEFAULT_HANDLERS],
+  modules: [AccessModule, QueueModule],
 });
 
 rester.bootstrap();
